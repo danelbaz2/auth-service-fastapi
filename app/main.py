369ea8a465f9aafe7c27dade337da259755aaf9d
danelbaz2 <danelbaz3@gmail.com>
2025-10-28 22:20:56 +0000

    add router

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
 from app.core.config import settings
+from app.api.routes.auth import router
 
 app = FastAPI(title="Auth Service (MVP)")
+
+app.include_router(router)
 
 @app.get("/health")
 def health():
